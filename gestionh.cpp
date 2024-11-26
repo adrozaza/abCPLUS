02b234e893e1c28211e44b01966b64ac73d77db2
adrozaza <adrianpp@msmk.university>
2024-11-26 20:38:55 +0100

    Add project files.

---
+++ gestionh.cpp
@@ -0,0 +1,12 @@
+﻿// gestionh.cpp : Defines the entry point for the application.
+//
+
+#include "gestionh.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
